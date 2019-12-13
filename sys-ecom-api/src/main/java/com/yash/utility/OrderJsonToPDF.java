d22e4c3146acdd7d53a35df99b93ecfa12b64b63
Yash-PradhanYashSharma <55781161+Yash-PradhanYashSharma@users.noreply.github.com>
2019-12-13 12:58:38 +0530

    Updated POM file, Changed Yash logo location and Updated Customer Id in Subscribe.

--- sys-ecom-api/src/main/java/com/yash/utility/OrderJsonToPDF.java
+++ sys-ecom-api/src/main/java/com/yash/utility/OrderJsonToPDF.java
@@ -1,6 +1,7 @@
 package com.yash.utility;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -26,21 +27,15 @@ import com.itextpdf.text.pdf.parser.clipper.Path;
 
 public class OrderJsonToPDF {
 
-
 	public byte[] jsontopdf(String filename, String content, String varr) throws IOException {
-
 		Document document = new Document();
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		
-
-		try
-
-		{
+		try {
 			int unitPrice = 0;
 			int totalPrice = 0;
 
 			String orderId = "";
-			String path = "D:/studio-workspace/sample/src/main/resources/images/yashlogo.png";
+			String path = "D:\\muleecomdemo\\sys-ecom-api-mule\\src\\main\\resources\\images\\yashlogo.jpg";
 			JSONObject jsonObject = new JSONObject(content);
 			JSONObject jsonVar = new JSONObject(varr);
 
@@ -50,31 +45,25 @@ public class OrderJsonToPDF {
 			table.setSpacingAfter(10f);
 			float[] columnWidths = { 4f, 4f, 4f };
 			table.setWidths(columnWidths);
-			
-			
 
-			
-			
 			Font f6 = new Font(FontFamily.UNDEFINED, 10.0f, Font.UNDEFINED, BaseColor.BLACK);
-			PdfPCell cellValue1 = new PdfPCell(new Phrase("Description",f6));
+			PdfPCell cellValue1 = new PdfPCell(new Phrase("Description", f6));
 			cellValue1.setHorizontalAlignment(Element.ALIGN_CENTER);
-			PdfPCell cellValue2 = new PdfPCell(new Phrase("UnitPrice",f6));
+			PdfPCell cellValue2 = new PdfPCell(new Phrase("UnitPrice", f6));
 			cellValue2.setHorizontalAlignment(Element.ALIGN_CENTER);
-			PdfPCell cellValue3 = new PdfPCell(new Phrase("Quantity",f6));
+			PdfPCell cellValue3 = new PdfPCell(new Phrase("Quantity", f6));
 			cellValue3.setHorizontalAlignment(Element.ALIGN_CENTER);
-			
+
 			table.addCell(cellValue1).setBorder(PdfPCell.NO_BORDER);
 			table.addCell(cellValue3).setBorder(PdfPCell.NO_BORDER);
-			table.addCell(cellValue2).setBorder(PdfPCell.NO_BORDER);	
-			
+			table.addCell(cellValue2).setBorder(PdfPCell.NO_BORDER);
 
+			// table.setHeaderRows(1);
+			/*
+			 * PdfPCell[] cells = table.getRow(0).getCells(); for (int j = 0; j <
+			 * cells.length; j++) { cells[j].setBackgroundColor(BaseColor.LIGHT_GRAY); }
+			 */
 
-			//table.setHeaderRows(1);
-			/*PdfPCell[] cells = table.getRow(0).getCells();
-			for (int j = 0; j < cells.length; j++) {
-				cells[j].setBackgroundColor(BaseColor.LIGHT_GRAY);
-			}*/
-			
 			PdfPTable table2 = new PdfPTable(3);
 			table2.setWidthPercentage(100);
 			table2.setSpacingBefore(10f);
@@ -82,35 +71,35 @@ public class OrderJsonToPDF {
 			table2.setWidths(columnWidths);
 			table2.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
 			table2.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-			//table2.getDefaultCell().setBackgroundColor(BaseColor.LIGHT_GRAY);
-			
+			// table2.getDefaultCell().setBackgroundColor(BaseColor.LIGHT_GRAY);
+
 			JSONArray arr = jsonObject.getJSONArray("orderItems");
 			Font f7 = new Font(FontFamily.UNDEFINED, 10.0f, Font.UNDEFINED, BaseColor.BLACK);
 
 			for (int i = 0; i < arr.length(); i++) {
-				
+
 				JSONObject json = arr.getJSONObject(i);
-				
-				PdfPCell cellValue4 = new PdfPCell(new Phrase((String) json.get("productId"),f7));
+
+				PdfPCell cellValue4 = new PdfPCell(new Phrase((String) json.get("productId"), f7));
 				cellValue4.setHorizontalAlignment(Element.ALIGN_CENTER);
-				//cellValue4.setBackgroundColor(BaseColor.LIGHT_GRAY);
-				PdfPCell cellValue5 = new PdfPCell(new Phrase(String.valueOf(json.get("unitPrice")),f7));
+				// cellValue4.setBackgroundColor(BaseColor.LIGHT_GRAY);
+				PdfPCell cellValue5 = new PdfPCell(new Phrase(String.valueOf(json.get("unitPrice")), f7));
 				cellValue5.setHorizontalAlignment(Element.ALIGN_CENTER);
-				//cellValue5.setBackgroundColor(BaseColor.LIGHT_GRAY);
-				PdfPCell cellValue6 = new PdfPCell(new Phrase(String.valueOf(json.get("quantity")),f7));
+				// cellValue5.setBackgroundColor(BaseColor.LIGHT_GRAY);
+				PdfPCell cellValue6 = new PdfPCell(new Phrase(String.valueOf(json.get("quantity")), f7));
 				cellValue6.setHorizontalAlignment(Element.ALIGN_CENTER);
-				//cellValue6.setBackgroundColor(BaseColor.LIGHT_GRAY);
-				
-				
+				// cellValue6.setBackgroundColor(BaseColor.LIGHT_GRAY);
+
 				table2.addCell(cellValue4).setBorder(PdfPCell.NO_BORDER);
-				table2.addCell(cellValue6).setBorder(PdfPCell.NO_BORDER);;
+				table2.addCell(cellValue6).setBorder(PdfPCell.NO_BORDER);
+				;
 				table2.addCell(cellValue5).setBorder(PdfPCell.NO_BORDER);
 				unitPrice = json.getInt("unitPrice");
 				totalPrice = totalPrice + (json.getInt("quantity") * unitPrice);
 				orderId = String.valueOf(json.get("orderId"));
 
 			}
-			
+
 			System.out.print("totalPrice" + jsonVar.getInt("orderTotal"));
 			PdfWriter.getInstance(document, out);
 
@@ -127,14 +116,16 @@ public class OrderJsonToPDF {
 			Image img = Image.getInstance(path);
 			document.add(img);
 			document.add(p4);
-			//document.add(new LineSeparator(0.5f, 100, null, 0, -5));
+			// document.add(new LineSeparator(0.5f, 100, null, 0, -5));
 			document.add(new Paragraph("\n"));
 			document.add(p);
 			document.add(new Paragraph("\n"));
-			//document.add(new Paragraph("Customer Name: " + jsonVar.getString("userName")));
-			//document.add(new Paragraph("\n\n"));
+			// document.add(new Paragraph("Customer Name: " +
+			// jsonVar.getString("userName")));
+			// document.add(new Paragraph("\n\n"));
 			document.add(new Paragraph("Dear " + jsonVar.getString("userName")
-					+ ",\nThank you for your order from Yash Website Store. Once your package ships we will send you tracking number.\n\nIf you have any queries about your order, you can email us at contact@yash.com.",f));
+					+ ",\nThank you for your order from Yash Website Store. Once your package ships we will send you tracking number.\n\nIf you have any queries about your order, you can email us at contact@yash.com.",
+					f));
 
 			document.add(new Paragraph("\n"));
 
@@ -158,20 +149,11 @@ public class OrderJsonToPDF {
 			document.add(new LineSeparator(0.5f, 100, null, 0, -5));
 
 			document.add(new Paragraph(
-					"Thanks for your purchase! \nFor any futher questions do not hesitate to contact us!",f));
-
+					"Thanks for your purchase! \nFor any futher questions do not hesitate to contact us!", f));
 			document.close();
-
-		} catch (
-
-		DocumentException e)
-
-		{
-
+		} catch (DocumentException e){
 			e.printStackTrace();
-
 		}
 		return out.toByteArray();
-
 	}
 }
